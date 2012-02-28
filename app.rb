51da2faa802b5b1748bf952cdc8e9998e4b36ccd
Brendan Baldwin <brendan@usergenic.com>
2012-02-27 21:44:38 -0800

    moved old site contents in here.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 
-get "/" do
-  "hi."
-end
+use Rack::Static, :root => "public", :urls => {
+  "/"               => "index.html",
+  "/resume/"        => "resume/index.html",
+  "/resume/ultima/" => "resume/ultima/index.html"
+}
 
+get "/resume" do
+  redirect "/resume/"
+end
+get "/resume/ultima" do
+  redirect "/resume/ultima/"
+end
