7f71b312228a295741ee880dbc267a018c94c275
Brendan Baldwin <brendan@usergenic.com>
2012-02-27 21:54:20 -0800

    removed rack static because it caused some kind of encoding issue on heroku routers.

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require "sinatra"
 
-use Rack::Static, :root => "public", :urls => {
-  "/"               => "index.html",
-  "/resume/"        => "resume/index.html",
-  "/resume/ultima/" => "resume/ultima/index.html"
-}
+get "/" do
+  File.read "public/index.html"
+end
+
+get "/resume/" do
+  File.read "public/resume/index.html"
+end
 
 get "/resume" do
   redirect "/resume/"
 end
+
+get "/resume/ultima/" do
+  File.read "public/resume/ultima/index.html"
+end
+
 get "/resume/ultima" do
   redirect "/resume/ultima/"
 end
